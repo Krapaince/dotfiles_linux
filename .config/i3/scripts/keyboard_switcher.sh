9315f230ffab525f787f6da83406980c106ea802
Krapaince <mathie2000@gmail.com>
2020-09-09 10:29:02 +0200

    [Add] Keyboard layout switcher

---
+++ .config/i3/scripts/keyboard_switcher.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+layout=`setxkbmap -query | grep 'layout' | tr -s [:space:] | cut -d" " -f 2`
+
+if  [[ $layout == "us" ]];then
+    echo "Layout set to fr"
+    setxkbmap fr
+else
+    setxkbmap us
+    echo "Layout set to us"
+fi

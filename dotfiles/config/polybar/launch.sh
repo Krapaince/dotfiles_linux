7b09940343e5d0efd8bda2b76bf6891159969ec6
Krapaince <mathieu.pointecouteau@epitech.eu>
2021-02-08 21:51:00 +0100

    Update polybar config files with Krapaince-arch

--- dotfiles/config/polybar/launch.sh
+++ dotfiles/config/polybar/launch.sh
@@ -8,24 +8,20 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 {%@@ if profile == "Krapaince-MASTER-arch" @@%}
-MONITOR=$MON_DP polybar left_top -c ~/.config/polybar/config.ini &
+MONITOR=$MON_DP polybar tertiary_top -c ~/.config/polybar/config.ini &
 
-MONITOR=$MON_HDMI polybar mid_top -c ~/.config/polybar/config.ini &
-MONITOR=$MON_HDMI polybar mid_bot -l info -c ~/.config/polybar/config.ini &
+MONITOR=$MON_HDMI polybar main_top -c ~/.config/polybar/config.ini &
+MONITOR=$MON_HDMI polybar main_bot -l info -c ~/.config/polybar/config.ini &
 
-MONITOR=$MON_DVI polybar right_top -c ~/.config/polybar/config.ini &
+MONITOR=$MON_DVI polybar secondary_top -c ~/.config/polybar/config.ini &
 
 {%@@ elif profile == "Krapaince-arch" @@%}
+for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+  if [[ ${m:0:3} == "eDP" ]]; then
+    MONITOR=$m polybar top -c ~/.config/polybar/config.ini &
+    MONITOR=$m polybar bottom -c ~/.config/polybar/config.ini &
+  else
+    MONITOR=$m polybar secondary_top -c ~/.config/polybar/config.ini &
+  fi
+done
 {%@@ endif @@%}
-
-## Launch bar1 and bar2
-#if type "xrandr"; then
-#  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-#    MONITOR=$m polybar top -c ~/.config/polybar/config.ini &
-#    MONITOR=$m polybar bottom -c ~/.config/polybar/config.ini &
-#  done
-#else
-#  polybar top -c ~/.config/polybar/config.ini &
-#  polybar bottom -c ~/.config/polybar/config.ini &
-#fi
-#

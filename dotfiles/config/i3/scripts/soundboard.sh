894211ba3fd8743c228464348f28819b48018407
Krapaince <mathieu.pointecouteau@epitech.eu>
2021-04-03 20:36:20 +0200

    Replace home hard path with ~

--- dotfiles/config/i3/scripts/soundboard.sh
+++ dotfiles/config/i3/scripts/soundboard.sh
@@ -7,6 +7,6 @@ cd $SOUNDBOARD_LOCATION
 
 while [ $VAR ]; do
 
-VAR=$(find . -iname "*$1*.*" | rofi -dmenu -matching fuzzy -i -theme /home/krapaince/.config/rofi/soundboard.rasi)
+VAR=$(find . -iname "*$1*.*" | rofi -dmenu -matching fuzzy -i -theme ~/.config/rofi/soundboard.rasi)
     play "$VAR"
 done

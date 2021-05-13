b075b6b24d29b00a91551871a875c00f6d6d914d
Krapaince <mathieu.pointecouteau@epitech.eu>
2021-05-13 21:12:27 +0200

    Improve Telescope config
    Add <C-j/k> to navigate in selection.
    Improve Telescope's bindings.

--- dotfiles/config/nvim/lua/config/telescope.lua
+++ dotfiles/config/nvim/lua/config/telescope.lua
@@ -3,11 +3,25 @@
 return function ()
   local map = require("utils").map
   local telescope = require('telescope')
+  local actions = require('telescope.actions')
 
   telescope.load_extension('fzy_native')
 
+  require('telescope').setup {
+    defaults = {
+      mappings = {
+        i = {
+          ["<C-j>"] = actions.move_selection_next,
+          ["<C-k>"] = actions.move_selection_previous,
+          ["<esc>"] = actions.close,
+        },
+      }
+    }
+  }
+
   map('n', '<C-p>', ':Telescope find_files<CR>')
-  map('n', '<C-f>', ':Telescope live_grep<CR>')
-  map('n', '<C-s>', ':Telescope lsp_references<CR>')
+  map('n', '/',     ':Telescope live_grep<CR>')
+  map('n', '<C-s>', ':Telescope lsp_document_symbols<CR>')
+  map('n', '<C-f>', ':Telescope current_buffer_fuzzy_find<CR>')
   map('n', '<C-d>', ':Telescope lsp_document_diagnostics<CR>')
 end

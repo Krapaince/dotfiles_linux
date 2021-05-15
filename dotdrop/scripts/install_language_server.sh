201f38de788db248f372b1eebc2eaadee79e92ca
Krapaince <mathieu.pointecouteau@epitech.eu>
2021-05-16 00:47:14 +0200

    Add missing language server installer script

---
+++ dotdrop/scripts/install_language_server.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# {{@@ header() @@}}
+
+# Install language server for neovim
+
+# Docker
+npm install -g dockerfile-language-server-nodejs
+
+# HTML
+npm install -g vscode-html-languageserver-bin
+
+# CSS
+npm install -g vscode-css-languageserver-bin
+
+# Json
+npm install -g vscode-json-languageserver
+
+# Bash
+npm i -g bash-language-server
+
+# Yaml
+npm install -g yaml-language-server

134e778fdfa0834404933336868d6505bdd133ee
Krapaince <mathieu.pointecouteau@epitech.eu>
2021-06-02 19:20:21 +0200

    Improve nvim-tree config.
    Disable special file and highlight opened files and folers.

--- dotfiles/config/nvim/lua/config/nvim-tree.lua
+++ dotfiles/config/nvim/lua/config/nvim-tree.lua
@@ -9,8 +9,10 @@ return function()
     '.git',
     '*.o'
   }
+  vim.g.nvim_tree_special_files = {}
+
   vim.g.nvim_tree_follow = 1
-  vim.g.nvim_tree_highlight_opened_files = 1
+  vim.g.nvim_tree_highlight_opened_files = 3
   vim.g.nvim_tree_indent_markers = 1
   vim.g.nvim_tree_lsp_diagnostics = 1
   vim.g.nvim_tree_tab_open = 1

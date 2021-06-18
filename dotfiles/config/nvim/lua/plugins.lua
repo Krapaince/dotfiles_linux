f46790fba4cb5ca91b67b5b5459cd066b50a0a57
Krapaince <mathieu.pointecouteau@epitech.eu>
2021-06-18 22:24:23 +0200

    Config packer to use a floating window.

--- dotfiles/config/nvim/lua/plugins.lua
+++ dotfiles/config/nvim/lua/plugins.lua
@@ -1,88 +1,95 @@
 -- {{@@ header() @@}}
 
-return require('packer').startup(function (use)
-    use 'wbthomason/packer.nvim'
+return require('packer').startup({
+    function (use)
+        use 'wbthomason/packer.nvim'
 
-    -- Colorschemes
-    use 'tomasiser/vim-code-dark'
-    -- use 'doums/darcula'
-    use 'kjwon15/vim-transparent'
+        -- Colorschemes
+        use 'tomasiser/vim-code-dark'
+        -- use 'doums/darcula'
+        use 'kjwon15/vim-transparent'
 
-    -- Icons
-    use 'kyazdani42/nvim-web-devicons'
+        -- Icons
+        use 'kyazdani42/nvim-web-devicons'
 
-    -- File Explorer
-    use { 'kyazdani42/nvim-tree.lua', config = require('config.nvim-tree') }
+        -- File Explorer
+        use { 'kyazdani42/nvim-tree.lua', config = require('config.nvim-tree') }
 
-    -- Status line
-    use { 'hoob3rt/lualine.nvim', config = require('config.lualine') }
+        -- Status line
+        use { 'hoob3rt/lualine.nvim', config = require('config.lualine') }
 
-    use { 'neovim/nvim-lspconfig',
-        requires = {
-            { 'RishabhRD/nvim-lsputils',
-                requires = 'RishabhRD/popfix',
+        use { 'neovim/nvim-lspconfig',
+            requires = {
+                { 'RishabhRD/nvim-lsputils',
+                    requires = 'RishabhRD/popfix',
+                },
             },
-        },
-        config = require('config.lsp')
+            config = require('config.lsp')
+        }
+
+        -- Lsp Diagnostics window
+        use {
+          'folke/lsp-trouble.nvim',
+          requires = 'kyazdani42/nvim-web-devicons',
+          config = require('config.lsp-trouble'),
+        }
+
+        -- Treesitter
+        use {
+          {
+            'nvim-treesitter/nvim-treesitter',
+            run = ':TSUpdate',
+            config = require('config.treesitter'),
+          },
+          'nvim-treesitter/playground'
+        }
+
+        -- Autocompletion
+        use { 'hrsh7th/nvim-compe', config = require('config.compe') }
+
+        use { 'hrsh7th/vim-vsnip', config = require('config.vsnip') }
+
+        -- Symbol tree
+        use { 'liuchengxu/vista.vim' }
+
+        -- Finder
+        use {
+            'nvim-telescope/telescope.nvim',
+            requires = {
+                {'nvim-lua/popup.nvim'},
+                {'nvim-lua/plenary.nvim'},
+                {'nvim-telescope/telescope-fzy-native.nvim', run = 'make'},
+            },
+            config = require('config.telescope')
+        }
+
+        use {
+            'iamcco/markdown-preview.nvim',
+            run = function() vim.fn['mkdp#util#install']() end,
+            ft = {'markdown'}
+        }
+
+        {%@@ if profile == "Krapaince-ubuntu" @@%}
+        use { 'kristijanhusak/vim-dadbod-ui',
+            requires = {{ 'tpope/vim-dadbod' }}
+        }
+        {%@@ endif @@%}
+
+        -- Utility plugins
+        use { 'ntpeters/vim-better-whitespace' } -- Highlight trailing space
+        use { 'wesQ3/vim-windowswap' }
+        use { 'vim-scripts/DrawIt' }
+        use { 'camspiers/animate.vim', config = require('config.animate') }
+        use { 'norcalli/nvim-colorizer.lua' } -- Color highlighter
+        require('config.nvim-colorizer')
+        use { 'tpope/vim-commentary', config = require('config.vim-commentary') }
+        use { 'pierreglaser/folding-nvim' }
+        use { 'junegunn/vim-easy-align' }
+        use { 'rhysd/git-messenger.vim', config = require('config.git-messenger') }
+    end,
+    config = {
+        display = {
+            open_fn = require('packer.util').float,
+        }
     }
-
-    -- Lsp Diagnostics window
-    use {
-      'folke/lsp-trouble.nvim',
-      requires = 'kyazdani42/nvim-web-devicons',
-      config = require('config.lsp-trouble'),
-    }
-
-    -- Treesitter
-    use {
-      {
-        'nvim-treesitter/nvim-treesitter',
-        run = ':TSUpdate',
-        config = require('config.treesitter'),
-      },
-      'nvim-treesitter/playground'
-    }
-
-    -- Autocompletion
-    use { 'hrsh7th/nvim-compe', config = require('config.compe') }
-
-    use { 'hrsh7th/vim-vsnip', config = require('config.vsnip') }
-
-    -- Symbol tree
-    use { 'liuchengxu/vista.vim' }
-
-    -- Finder
-    use {
-        'nvim-telescope/telescope.nvim',
-        requires = {
-            {'nvim-lua/popup.nvim'},
-            {'nvim-lua/plenary.nvim'},
-            {'nvim-telescope/telescope-fzy-native.nvim', run = 'make'},
-        },
-        config = require('config.telescope')
-    }
-
-    use {
-        'iamcco/markdown-preview.nvim',
-        run = function() vim.fn['mkdp#util#install']() end,
-        ft = {'markdown'}
-    }
-
-    {%@@ if profile == "Krapaince-ubuntu" @@%}
-    use { 'kristijanhusak/vim-dadbod-ui',
-        requires = {{ 'tpope/vim-dadbod' }}
-    }
-    {%@@ endif @@%}
-
-    -- Utility plugins
-    use { 'ntpeters/vim-better-whitespace' } -- Highlight trailing space
-    use { 'wesQ3/vim-windowswap' }
-    use { 'vim-scripts/DrawIt' }
-    use { 'camspiers/animate.vim', config = require('config.animate') }
-    use { 'norcalli/nvim-colorizer.lua' } -- Color highlighter
-    require('config.nvim-colorizer')
-    use { 'tpope/vim-commentary', config = require('config.vim-commentary') }
-    use { 'pierreglaser/folding-nvim' }
-    use { 'junegunn/vim-easy-align' }
-    use { 'rhysd/git-messenger.vim', config = require('config.git-messenger') }
-end)
+})

024d9ffeeade51c592714007d273af3b6bb262e2
Krapaince <mathieu.pointecouteau@epitech.eu>
2021-06-30 18:52:48 +0200

    Fix PATH variable in fish config.

--- dotfiles/config/omf/init.fish
+++ dotfiles/config/omf/init.fish
@@ -11,14 +11,6 @@ set -g REPO_LOCATION "$HOME/Desktop"
 set -x SOUNDBOARD_LOCATION ~/Music/Soundboard/
 {%@@ endif @@%}
 
-set -g NPM_PACKAGES ~/.npm-global/bin
-set -g LOCAL_BIN ~/.local/bin
-set -g VIM_SCRIPT ~/.config/nvim/scripts/
-set -g CARGO_BIN ~/.cargo/bin
-
-set -x PATH $PATH:$LOCAL_BIN:$VIM_SCRIPT:$CARGO_BIN:$NPM_PACKAGES
-{%@@ if profile == "Krapaince-ubuntu" @@%}
-set -g PATH $PATH:/snap/bin
-{%@@ endif @@%}
+set PATH ~/.npm-global/bin ~/.local/bin ~/.config/nvim/scripts/ ~/.cargo/bin $PATH
 
 source ~/.config/omf/alias.fish

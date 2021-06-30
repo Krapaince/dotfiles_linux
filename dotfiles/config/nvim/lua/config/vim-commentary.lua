5908ad6ccc475c3d1e7d950555d6493bd078a99b
Krapaince <mathieu.pointecouteau@epitech.eu>
2021-06-30 18:52:25 +0200

    Fix nvim config.

--- dotfiles/config/nvim/lua/config/vim-commentary.lua
+++ dotfiles/config/nvim/lua/config/vim-commentary.lua
@@ -3,6 +3,6 @@
 return function ()
     local map = require('utils').map
 
-    map('n', '<space>/', ':Commentary<CR>')
-    map('v', '<space>/', ':Commentary<CR>')
+    map('n', '<Space>/', ':Commentary<CR>')
+    map('v', '<Space>/', ':Commentary<CR>')
 end

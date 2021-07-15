28fe7215fff93fb27aaa92a1f713764e7999aaec
Krapaince <mathieu.pointecouteau@epitech.eu>
2021-07-15 22:00:51 +0200

    Add custom i3lock script.

--- dotfiles/config/i3/scripts/lock.sh
+++ dotfiles/config/i3/scripts/lock.sh
@@ -5,5 +5,29 @@
 setxkbmap us
 
 killall -SIGUSR1 dunst
-i3lock -ti {{@@ wallpaper @@}} -n
+i3lock \
+    -ti {{@@ wallpaper @@}} \
+    -n \
+    --clock \
+    --time-str="%H:%M" \
+    --time-color="AAAAAA" \
+    --date-str="%a %d %Y" \
+    --date-color="fefedf" \
+    --time-color="fefedf" \
+    --radius 75 \
+    --ind-pos='x+942:y+425' \
+    --ring-width 4 \
+    --ringver-color="00000000" \
+    --ringwrong-color="00000000" \
+    --ring-color="00000000" \
+    --verif-color="71b84e" \
+    --wrong-color="ae5043" \
+    --keyhl-color="fefedf" \
+    --bshl-color="fc7e7e" \
+    --line-color="00000000" \
+    --insidewrong-color="00000000" \
+    --insidever-color="00000000" \
+    --inside-color="00000000" \
+    --noinput-text=""
+
 killall -SIGUSR2 dunst
--- dotfiles/config/i3/scripts/suspend.sh
+++ dotfiles/config/i3/scripts/suspend.sh
@@ -4,9 +4,4 @@
 # Reset the main keyboard layout
 setxkbmap us
 
-i3lock -ti {{@@ wallpaper @@}} -n &
-
-# Prevent the screen flash when exiting the suspend mode
-/usr/bin/sleep 1
-
-systemctl suspend
+systemctl suspend && ~/.config/i3/scripts/lock.sh

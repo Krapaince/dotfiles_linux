2774f6e26399c508f2ae2cfb9d4664284305b79c
Krapaince <mathieu.pointecouteau@epitech.eu>
2021-09-12 02:04:08 +0200

    Update alias.sh.

--- dotfiles/config/omf/alias.fish
+++ dotfiles/config/omf/alias.fish
@@ -5,22 +5,17 @@ alias c="clear"
 alias ls="exa --icons"
 
 ## Go to
-alias cdd="cd $REPO_LOCATION"
 alias cdc="cd ~/.config"
 
+alias cdd="cd $REPO_LOCATION"
 alias cde="cd $REPO_LOCATION/EPITECH/"
 alias cdg="cd $REPO_LOCATION/GIT/"
 alias cdw="cd $REPO_LOCATION/EPITECH/delivery"
+alias cdp="cd $REPO_LOCATION/EPITECH/EIP/Pizzi"
 
 ## Git
 alias gs="git status"
 alias gl="git log"
-
-## Makefile
-alias m="make -j"
-alias mc="make clean"
-alias mf="make fclean"
-alias mt="make tests_run -j"
 
 ##Valgrind
 alias valgrindf="valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose --dsymutil=yes --num-callers=500 --log-file=valgrind.txt"
@@ -29,3 +24,7 @@ alias valgrind-colour="cat valgrind.txt | colout -t valgrind | less"
 #clang-format
 alias clang-format-all="find . -name \"*.hpp\" -print -o -name \"*.cpp\" -print -o -name \"*.c\" -print -o -name \"*.h\" -print | xargs clang-format -i --verbose"
 alias formatter="/usr/bin/clang-format -style=file -fallback-style=none -assume-filename="
+
+if test -f custom_alias.fish.fish
+    source ~/.config/omf/custom_alias.fish
+end

51d3a5bd62762de3dbc3d515f91e7902ce7a1eeb
Krapaince <mathieu.pointecouteau@epitech.eu>
2021-09-27 16:08:45 +0200

    Update nvim-tree config.

--- dotfiles/config/nvim/lua/plugins/nvim-tree.lua
+++ dotfiles/config/nvim/lua/plugins/nvim-tree.lua
@@ -3,24 +3,25 @@
 return function()
   local map = require("utils").map
 
-  map('n', '<C-b>', ':NvimTreeToggle<CR>')
+  vim.g.nvim_tree_special_files = {}
 
-  local callbacks = require'nvim-tree.config'.nvim_tree_callback
-  vim.g.nvim_tree_bindings = {
-      { key = "p", cb = callbacks("parent_node") },
-  }
+  vim.g.nvim_tree_highlight_opened_files = 3
+  vim.g.nvim_tree_indent_markers = 1
+  vim.g.nvim_tree_group_empty = 0
 
   vim.g.nvim_tree_ignore = {
     '.git',
     '*.o'
   }
-  vim.g.nvim_tree_special_files = {}
 
-  vim.g.nvim_tree_follow = 1
-  vim.g.nvim_tree_highlight_opened_files = 3
-  vim.g.nvim_tree_indent_markers = 1
-  vim.g.nvim_tree_lsp_diagnostics = 1
-  vim.g.nvim_tree_tab_open = 1
-  vim.g.nvim_tree_auto_resize = 0
-  vim.g.nvim_tree_group_empty = 0
+  require'nvim-tree'.setup {
+      open_on_tab = false,
+      lsp_diagnostics = true,
+      view = {
+          side = 'left',
+          auto_resize = false,
+      }
+  }
+
+  map('n', '<C-b>', ':NvimTreeToggle<CR>')
 end

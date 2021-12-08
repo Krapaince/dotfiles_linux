6d78b30c41125de6d53a17b4e09200b116785a10
Krapaince <mathieu.pointecouteau@epitech.eu>
2021-12-08 03:05:22 +0100

    Update tsc config (automatic formatter picking).

--- dotfiles/config/nvim/lua/plugins/lspconfig/tsserver.lua
+++ dotfiles/config/nvim/lua/plugins/lspconfig/tsserver.lua
@@ -1,54 +1,27 @@
+-- {{@@ header() @@}}
 local nvim_lsp = require("lspconfig")
+local null_ls = require("null-ls")
 
 -- enable null-ls integration (optional)
-require("null-ls").config {}
-require("lspconfig")["null-ls"].setup {}
+null_ls.config({
+  sources = {
+    require("null-ls.helpers").conditional(function(utils)
+      return utils.root_has_file(".eslintrc.js") and
+      null_ls.builtins.formatting.eslint_d or
+      null_ls.builtins.formatting.prettier
+    end),
+  }
+})
+require("lspconfig")["null-ls"].setup({})
 
-nvim_lsp.tsserver.setup {
+nvim_lsp.tsserver.setup ({
+    init_options = require("nvim-lsp-ts-utils").init_options,
     on_attach = function(client, bufnr)
         -- disable tsserver formatting if you plan on formatting via null-ls
         client.resolved_capabilities.document_formatting = false
         client.resolved_capabilities.document_range_formatting = false
 
         local ts_utils = require("nvim-lsp-ts-utils")
-
-        ts_utils.setup {
-            debug = false,
-            disable_commands = false,
-            enable_import_on_completion = false,
-
-            -- import all
-            import_all_timeout = 5000, -- ms
-            import_all_priorities = {
-                buffers = 4,            -- loaded buffer names
-                buffer_content = 3,     -- loaded buffer content
-                local_files = 2,        -- git files or files with relative path markers
-                same_file = 1,          -- add to existing import statement
-            },
-            import_all_scan_buffers = 100,
-            import_all_select_source = false,
-
-            -- eslint
-            eslint_enable_code_actions = true,
-            eslint_enable_disable_comments = true,
-            eslint_bin = "eslint",
-            eslint_enable_diagnostics = false,
-            eslint_opts = {},
-
-            -- formatting
-            enable_formatting = true,
-            formatter = "prettier",
-            formatter_opts = {},
-
-            -- update imports on file move
-            update_imports_on_move = false,
-            require_confirmation_on_move = false,
-            watch_dir = nil,
-
-            -- filter diagnostics
-            filter_out_diagnostics_by_severity = {},
-            filter_out_diagnostics_by_code = {},
-        }
 
         -- required to fix code action ranges and filter diagnostics
         ts_utils.setup_client(client)
@@ -71,4 +44,4 @@ nvim_lsp.tsserver.setup {
         buf_set_keymap("n", "<leader>f", "<cmd>lua vim.lsp.buf.formatting()<CR>")
         vim.cmd 'autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()'
     end
-}
+})

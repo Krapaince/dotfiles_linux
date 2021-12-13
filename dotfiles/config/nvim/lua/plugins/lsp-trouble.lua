6b8c6d87f68361b6a2408128db7677e913c90dea
Krapaince <mathieu.pointecouteau@epitech.eu>
2021-12-13 21:25:26 +0100

    Update the deprecated setup of nvim lsp-trouble plugin.

--- dotfiles/config/nvim/lua/plugins/lsp-trouble.lua
+++ dotfiles/config/nvim/lua/plugins/lsp-trouble.lua
@@ -2,7 +2,7 @@ return function ()
   local map = require('utils').map
 
   require('trouble').setup {
-    use_lsp_diagnostic_signs = true,
+    use_diagnostic_signs = true,
   }
 
   map('n', '<C-d>', ':LspTroubleToggle<CR>')

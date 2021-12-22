791744183eb8b56fc4b86d38f497ddf8405413f9
Krapaince <mathieu.pointecouteau@epitech.eu>
2021-12-22 14:22:17 +0100

    Update Krapaince-MASTER-arch screen layout.

--- dotfiles/config/i3/scripts/monitor/reload_screen_setup.sh
+++ dotfiles/config/i3/scripts/monitor/reload_screen_setup.sh
@@ -2,9 +2,9 @@
 # {{@@ header() @@}}
 
 {%@@ if profile == "Krapaince-MASTER-arch" @@%}
-xrandr  --output $MON_DVI  --off \
-        --output $MON_HDMI --mode 1920x1080 --pos 1280x0 --rotate normal \
-        --output $MON_DP   --mode 1280x1024 --pos 3840x0 --rotate normal
+xrandr --output $MON_DVI  --off \
+       --output $MON_HDMI --mode 1920x1080 --pos 1280x0 --rotate normal \
+       --output $MON_DP   --mode 1280x1024 --pos 0x0    --rotate normal
 {%@@ endif @@%}
 ~/.config/i3/scripts/monitor/setup_screen.sh
 ~/.config/i3/scripts/set_wallpaper.sh

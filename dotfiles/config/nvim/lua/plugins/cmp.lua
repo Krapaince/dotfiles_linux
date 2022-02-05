3d72d7a43eddee5f54c69d9af557f51e26338945
Krapaince <mathieu.pointecouteau@epitech.eu>
2022-02-06 00:40:26 +0100

    Update nvim lspkind confi.

--- dotfiles/config/nvim/lua/plugins/cmp.lua
+++ dotfiles/config/nvim/lua/plugins/cmp.lua
@@ -34,10 +34,38 @@ return function()
     },
 
     formatting = {
-      format = function(entry, vim_item)
-        vim_item.kind = lspkind.presets.default[vim_item.kind]
-        return vim_item
-      end
+      format = lspkind.cmp_format({
+        mode = 'symbol',
+        maxwidth = '50',
+
+        symbol_map = {
+          Text = "",
+          Method = "",
+          Function = "",
+          Constructor = "",
+          Field = "ﴲ",
+          Variable = "[]",
+          Class = "",
+          Interface = "ﰮ",
+          Module = "",
+          Property = "襁",
+          Unit = "",
+          Value = "",
+          Enum = "練",
+          Keyword = "",
+          Snippet = "",
+          Color = "",
+          File = "",
+          Reference = "",
+          Folder = "",
+          EnumMember = "",
+          Constant = "ﲀ",
+          Struct = "ﳤ",
+          Event = "",
+          Operator = "",
+          TypeParameter = ""
+        }
+      })
     }
   }
 

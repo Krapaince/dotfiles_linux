5825b4aeb073d924df29201f67108ff9709649c1
Krapaince <Krapaince@pm.me>
2022-05-15 01:23:16 +0200

    Add UI config for telescope.

--- dotfiles/config/nvim/lua/plugins/telescope.lua
+++ dotfiles/config/nvim/lua/plugins/telescope.lua
@@ -7,6 +7,24 @@ return function()
 
   require('telescope').setup({
     defaults = {
+      prompt_prefix = '   ',
+      selection_caret = ' ',
+      entry_prefix = ' ',
+      sorting_strategy = 'ascending',
+      layout_strategy = 'horizontal',
+      layout_config = {
+        horizontal = {
+          prompt_position = 'top',
+          preview_width = 0.55,
+          results_width = 0.8,
+        },
+        vertical = {
+          mirror = false,
+        },
+        width = 0.80,
+        height = 0.85,
+        preview_cutoff = 120,
+      },
       mappings = {
         i = {
           ['<C-j>'] = actions.move_selection_next,

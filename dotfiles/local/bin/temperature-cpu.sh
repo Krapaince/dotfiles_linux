3d7a3832fc1ffa4d940b744c02ce8a849f2cae9c
Krapaince <Krapaince@pm.me>
2022-09-14 00:32:28 +0200

    Handle Intel and AMD cpu manufacturers in temp CPU script.

--- dotfiles/local/bin/temperature-cpu.sh
+++ dotfiles/local/bin/temperature-cpu.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 # {{@@ header() @@}}
-# This dotfile is managed using dotdrop
 
+{%@@ if cpu_manufacturer == "AMD" @@%}
+temp=$(sensors | grep "Tctl" | cut -d ' ' -f 10 | cut -d '+' -f 2)
+echo ${temp::-2}
+{%@@ else @@%}
 sensors | grep "Package id" | cut -d ' ' -f 5 | cut -d '+' -f 2 | cut -d '.' -f 1
+{%@@ endif @@%}

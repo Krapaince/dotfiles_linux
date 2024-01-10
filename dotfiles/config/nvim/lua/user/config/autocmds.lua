0ba9975e3d534bad6b76e2913b8830e6f325d57c
Krapaince <Krapaince@pm.me>
2024-01-10 21:06:02 +0100

    Enable spell check in LaTeX file.

--- dotfiles/config/nvim/lua/user/config/autocmds.lua
+++ dotfiles/config/nvim/lua/user/config/autocmds.lua
@@ -22,7 +22,7 @@ vim.api.nvim_create_autocmd('FileType', {
 -- wrap and check for spell in text filetypes
 vim.api.nvim_create_autocmd('FileType', {
   group = augroup('wrap_spell'),
-  pattern = { 'gitcommit', 'markdown' },
+  pattern = { 'gitcommit', 'markdown', 'latex', 'tex' },
   callback = function()
     vim.opt_local.wrap = true
     vim.opt_local.spell = true

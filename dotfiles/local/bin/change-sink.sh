ff95f4b3d954a42e7c9f26e049dfca6ba177f849
Krapaince <Krapaince@pm.me>
2024-01-10 21:02:11 +0100

    Update sink switcher script.

--- dotfiles/local/bin/change-sink.sh
+++ dotfiles/local/bin/change-sink.sh
@@ -2,20 +2,39 @@
 # {{@@ header() @@}}
 # https://gist.github.com/jkcdarunday/617a0f6662726bd85a71e72969c54128
 
-SINKS=$(pactl list short sinks)
+# Author: Jan Keith Darunday <github@jkcdarunday.mozmail.com>
+# Description: A shell script that switches to the next available Pulseaudio output device/sink
+# Note: This uses pactl instead of pacmd since pacmd is not available in pipewire
+
+function get_current_sink() {
+  pactl info | sed -En 's/Default Sink: (.*)/\1/p'
+}
+
+SINKS=$(pactl list short sinks | grep -v easyeffects)
 SINK_COUNT=$(echo "$SINKS" | wc -l)
 
-DEFAULT_SINK=$(pactl info | sed -En 's/Default Sink: (.*)/\1/p')
-DEFAULT_SINK_INDEX=$(echo "$SINKS" | grep -n "$DEFAULT_SINK" | egrep -o '^[0-9]+')
+CURRENT_SINK=$(get_current_sink)
+CURRENT_SINK_INDEX=$(echo "$SINKS" | grep -n "$CURRENT_SINK" | grep -Eo '^[0-9]+')
 
-NEW_SINK_INDEX=$((DEFAULT_SINK_INDEX % $SINK_COUNT + 1))
-NEW_SINK=$(echo "$SINKS" | sed "${NEW_SINK_INDEX}q;d" | awk '{ print $2 }')
+MAX_RETRIES=6
+RETRIES=0
 
-# Set next sink as the default sink
-pactl set-default-sink "$NEW_SINK"
+while true; do
+  [ "$RETRIES" -ge "$MAX_RETRIES" ] && echo "Reached retry limit of $MAX_SINK_SKIPS, giving up." && break
 
-# Forward all playing audio (sink inputs) to the new sink
-SINK_INPUTS=($(pactl list short sink-inputs | egrep -o '^[0-9]+'))
-for SINK_INPUT in ${SINK_INPUTS[*]}; do
-  pactl move-sink-input $SINK_INPUT $NEW_SINK
+  NEW_SINK_INDEX=$(((CURRENT_SINK_INDEX + $RETRIES) % $SINK_COUNT + 1))
+  NEW_SINK=$(echo "$SINKS" | sed "${NEW_SINK_INDEX}q;d" | awk '{ print $2 }')
+
+  echo "Switching to sink: $NEW_SINK"
+  pactl set-default-sink "$NEW_SINK"
+
+  [ "$(get_current_sink)" = "$NEW_SINK" ] && break
+
+  # Note: switching could fail if, for example, the new sink does not have any available output port
+  echo "Failed to switch to sink: $NEW_SINK, skipping to next sink..."
+  RETRIES=$((RETRIES + 1))
 done
+
+# Forward all playing audio (sink inputs) to the new sink (Uncomment if your system does not automatically do this)
+SINK_INPUTS=($(pactl list short sink-inputs | grep -Eo '^[0-9]+'))
+for SINK_INPUT in ${SINK_INPUTS[*]}; do pactl move-sink-input $SINK_INPUT $NEW_SINK; done

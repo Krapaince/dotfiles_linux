5ede6126c8352d4773a4b480d367e5347d28daf6
Krapaince <Krapaince@pm.me>
2024-01-10 21:01:20 +0100

    Update wezterm font size and scrollback_lines config.

--- dotfiles/config/wezterm/wezterm.lua
+++ dotfiles/config/wezterm/wezterm.lua
@@ -6,13 +6,14 @@ require('tab-title')
 
 local config = {
   automatically_reload_config = true,
-  font = wezterm.font('Iosevka Term Extended', { stretch = 'Expanded' }),
-  font_size = 9,
+  font = wezterm.font('Iosevka Term Extended'),
+  font_size = 8,
   line_height = 1,
-  cell_width = 0.9,
+  cell_width = 0.8,
   hide_tab_bar_if_only_one_tab = true,
   switch_to_last_active_tab_when_closing_tab = true,
   use_fancy_tab_bar = false,
+  scrollback_lines = 5000,
 
   inactive_pane_hsb = {
     saturation = 1.0,

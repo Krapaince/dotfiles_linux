42388a3374d37734a1385a1af04d11fc9a11f4fc
Krapaince <Krapaince@pm.me>
2024-01-10 20:59:02 +0100

    Use satty copy option instead of screenshot saving.

--- dotfiles/config/hypr/scripts/screenshot.sh
+++ dotfiles/config/hypr/scripts/screenshot.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 
-SCREENSHOT_FILEPATH="$HOME/Pictures/screenshots/satty-$(date '+%Y%m%d-%H:%M:%S').png"
-
-mkdir -p "$(dirname "$SCREENSHOT_FILEPATH")"
-
-slurp | grim -g - - | satty --filename - --output-filename "$SCREENSHOT_FILEPATH" --early-exit
-wl-copy < "$SCREENSHOT_FILEPATH"
+slurp | grim -g - - | satty --filename - --copy-command wl-copy --early-exit

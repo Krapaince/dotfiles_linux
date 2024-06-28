9b767d17f00533432671a66620cb7a789c82af88
Krapaince <Krapaince@pm.me>
2024-06-28 14:06:55 +0200

    Disable lua_ls formatter.

--- dotfiles/config/nvim/lua/user/plugins/lsp/servers/lua_ls.lua
+++ dotfiles/config/nvim/lua/user/plugins/lsp/servers/lua_ls.lua
@@ -20,6 +20,7 @@ M.setup = function(on_attach, capabilities)
         diagnostics = {
           globals = { 'vim', 'use' },
         },
+        format = { enable = false },
         workspace = {
           library = vim.api.nvim_get_runtime_file('', true),
         },
